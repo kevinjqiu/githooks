b1b08760d241a5bf3909fdbb80619bf5c9a9c1de
Kevin Jing Qiu <kevin@idempotent.ca>
2017-01-27 00:27:41 -0500

    Add help and remove bash prepare_commit_msg script

--- hooks/githooks.py
+++ hooks/githooks.py
@@ -1,10 +1,10 @@
+import re
 import functools
 import subprocess
+from collections import defaultdict
 
 
-HOOKS = {
-    'prepare-commit-msg': []
-}
+HOOKS = defaultdict(list)
 
 
 def get_active_hooks(hook_type):
@@ -22,11 +22,22 @@ def prepare_commit_msg_hook(fn):
 
 @prepare_commit_msg_hook
 def append_jira_ticket_id(temp_msg_file):
+    """If you name your branch using the following scheme:
+
+        issue-NNNNN-description
+        JIRAPROJECT-NNNNN-description
+
+    this prepare_commit_msg hook will prepend your comment message
+    with `issue-NNNNN` or `JIRAPROJECT-NNNNN`
+    """
     branch_name = subprocess.check_output('git symbolic-ref --short HEAD', shell=True).strip()
     if '-' not in branch_name:
         return
-    jira_ticket, _ = branch_name.split('-', 2)
-    with open(temp_msg_file, 'rw') as fh:
+    parts = branch_name.split('-')
+    jira_ticket = '-'.join(parts[:2])
+    with open(temp_msg_file, 'r') as fh:
         msg = fh.read()
-        msg = '{}: {}'.format(jira_ticket, msg)
+
+    msg = '{}: {}'.format(jira_ticket, msg)
+    with open(temp_msg_file, 'w') as fh:
         fh.write(msg)
