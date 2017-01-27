cd2c10ac4df10509f5f1cd42d7a58528ad0cbb2d
Kevin Jing Qiu <kevin@idempotent.ca>
2017-01-26 23:23:13 -0500

    dispatch actions

--- hooks/main.py
+++ hooks/main.py
@@ -4,6 +4,7 @@ hooks - git hooks
 Usage:
     hooks install
     hooks ls
+    hooks update
 """
 
 from docopt import docopt
@@ -12,6 +13,27 @@ from docopt import docopt
 __version__ = '0.1'
 
 
+def install(args):
+    pass
+
+
+def update(args):
+    pass
+
+
+def ls(args):
+    pass
+
+
+def dispatch(arguments):
+    if arguments['install']:
+        install(arguments)
+    elif arguments['update']:
+        update(arguments)
+    elif arguments['ls']:
+        ls(arguments)
+
+
 def cli():
     arguments = docopt(__doc__, version=__version__)
-    print(arguments)
+    dispatch(arguments)
