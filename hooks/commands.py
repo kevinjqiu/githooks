4920cbb54ac48f47a7b13a7711b1776b2bd49711
Kevin Jing Qiu <kevin@idempotent.ca>
2017-01-27 14:15:34 -0500

    Implement -f

--- hooks/commands.py
+++ hooks/commands.py
@@ -2,27 +2,21 @@ import shutil
 import os
 
 
-def dispatch(arguments):
-    command = arguments['COMMAND']
-    try:
-        if command == 'install':
-            return install(arguments)
-        elif command == 'update':
-            return update(arguments)
-        elif command == 'ls':
-            return ls(arguments)
-        else:
-            raise RuntimeError('No action specified')
-    except RuntimeError as e:
-        print(str(e))
-        return 1
-
-
 def _ensure_cwd_is_a_git_repo():
     """Is the current working directory a git repo?"""
     dot_git = os.path.join(os.getcwd(), '.git')
     if not (os.path.exists(dot_git) and os.path.isdir(dot_git)):
         raise RuntimeError('Must be in a git repository')
+
+
+def _copy_file(src, dest, use_force):
+    if os.path.exists(dest) and not use_force:
+        user_input = raw_input('{} exists. \n'
+                               'Do you want to overwrite it? [Y/n] '.format(dest))
+        if user_input.lower() == 'n':
+            return False
+    shutil.copy(src, dest)
+    return True
 
 
 def install(args):
@@ -34,12 +28,13 @@ Usage:
 
 Options:
     -f  Overwrite without prompt when the hook already exists."""
+    use_force = args.get('-f', False)
     _ensure_cwd_is_a_git_repo()
     git_hook_directory = os.path.join(os.getcwd(), '.git', 'hooks')
     source = os.path.join(os.path.dirname(__file__), 'githooks', 'prepare-commit-msg')
     dest = os.path.join(git_hook_directory, 'prepare-commit-msg')
-    shutil.copy(source, dest)
-    print('prepare-commit-msg hook installed')
+    if _copy_file(source, dest, use_force):
+        print('prepare-commit-msg hook installed')
 
 
 def update(args):
