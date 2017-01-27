2f3950628a3ea0d1b20a0713bd1a8572a076f6bb
Kevin Jing Qiu <kevin@idempotent.ca>
2017-01-27 12:02:40 -0500

    Implement install

--- hooks/commands.py
+++ hooks/commands.py
@@ -7,13 +7,26 @@ def dispatch(arguments):
         elif arguments['ls']:
             return ls(arguments)
         else:
-            return 1
-    except:
+            raise RuntimeError('No action specified')
+    except RuntimeError as e:
+        print(str(e))
         return 1
 
 
+def _ensure_cwd_is_a_git_repo():
+    """Is the current working directory a git repo?"""
+    dot_git = os.path.join(os.getcwd(), '.git')
+    if not (os.path.exists(dot_git) and os.path.isdir(dot_git)):
+        raise RuntimeError('Must be in a git repository')
+
+
 def install(args):
-    pass
+    _ensure_cwd_is_a_git_repo()
+    git_hook_directory = os.path.join(os.getcwd(), '.git', 'hooks')
+    source = os.path.join(os.path.dirname(__file__), 'githooks', 'prepare-commit-msg')
+    dest = os.path.join(git_hook_directory, 'prepare-commit-msg')
+    shutil.copy(source, dest)
+    print('prepare-commit-msg hook installed')
 
 
 def update(args):
